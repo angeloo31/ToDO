d10915e94d04119d04438dcac076fa7d5b225ac5
angeloo31 <121607721+angeloo31@users.noreply.github.com>
2025-03-13 22:06:22 +0100

    Completed exercise

---
+++ src/ToDo.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./style.css";
+function Todo() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState([]);
+
+  function generateid() {
+    return Math.floor(Math.random() * 1000);
+  }
+
+  const handleSubmit = () => {
+    setTodos((todos) =>
+      todos.concat({
+        text: input,
+        id: generateid(),
+      })
+    );
+    setInput("");
+  };
+
+  const removeTodo = (id) =>
+    setTodos((todos) => todos.filter((t) => t.id !== id));
+
+  return (
+    <>
+      <div className="container">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="New Todo"
+        />
+        <button onClick={handleSubmit}>Submit</button>
+        <ul className="todos-list">
+          {todos.map(({ text, id }) => (
+            <li key={id} className="todo">
+              <span>{text}</span>
+              <button className="close" onClick={() => removeTodo(id)}>
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Todo;
